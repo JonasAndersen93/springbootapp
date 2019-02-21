5e8794012a4e6c9716b01fd3f58c41b06e005894
Jonas Andersen <joa@miracle.dk>
2019-02-21 15:29:49 +0100

    commit first files

---
+++ src/main/java/com/boot/config/PersistenceConfiguration.java
@@ -0,0 +1,29 @@
+package com.boot.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource")
+    @Primary // Fordi det er vores default datasource vi ønsker at bruge/konfigurere
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+/*
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.flyway")
+    @FlywayDataSource
+    public DataSource flywayDataSource(){
+        return DataSourceBuilder.create().build();
+    }*/
+
+}
